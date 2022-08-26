0ff7f7c8d02dc8aca4b5dc121ba972d10e6f4717
ivaylonikolov7 <ivnikolov721@gmail.com>
2022-08-26 14:51:16 +0300

    feat: add deploy function

---
+++ batch-nft-reveal/app/lib/deploy.ts
@@ -0,0 +1,37 @@
+import { ContractFactory, Signer } from 'ethers'
+import { TransactionReceipt } from '@ethersproject/abstract-provider/lib/index'
+
+import NFTCollection from '../artifacts/contracts/NFTCollection.sol/NFTCollection.json'
+import {
+  VRF_CALLBACK_GAS_LIMIT,
+  VRF_COORDINATOR_V2_ADDRESS,
+  VRF_GAS_LANE,
+} from '../conf/config'
+import NFTCollectionParams from '../types/NFTCollectionParams'
+
+export async function deployNFTCollection(
+  nftParams: NFTCollectionParams,
+  signer: Signer,
+  chainId: number
+): Promise<TransactionReceipt> {
+  const nftCollection = new ContractFactory(
+    NFTCollection.abi,
+    NFTCollection.bytecode,
+    signer
+  )
+  const deployedContract = await nftCollection.deploy(
+    nftParams.nftName,
+    nftParams.nftSymbol,
+    nftParams.nftMaxSupply,
+    nftParams.nftMintCost,
+    nftParams.nftRevealBatchSize,
+    nftParams.nftRevealInterval,
+    VRF_COORDINATOR_V2_ADDRESS[chainId],
+    nftParams.vrfSubscriptionId,
+    VRF_GAS_LANE[chainId],
+    VRF_CALLBACK_GAS_LIMIT
+  )
+
+  const tx = await deployedContract.deployTransaction.wait(1)
+  return tx
+}
