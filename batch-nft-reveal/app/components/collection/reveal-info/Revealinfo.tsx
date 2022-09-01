cabb50909349dd478eff06bd3a95b46a755bb9da
ivaylonikolov7 <ivnikolov721@gmail.com>
2022-09-01 22:39:51 +0300

    feat(frontend): add isPending function

--- batch-nft-reveal/app/components/collection/reveal-info/Revealinfo.tsx
+++ batch-nft-reveal/app/components/collection/reveal-info/Revealinfo.tsx
@@ -10,11 +10,36 @@ export const RevealInfo = (props: AddressProp): JSX.Element => {
 
   const nextRevealBatchSize = useNextRevealAmount(contractAddress)
   const nextRevealTime = useNextRevealTime(contractAddress)
-  const isPending = useContractCall('pendingReveal', [], contractAddress)
-
+  const revealedCount: number = useContractCall(
+    'revealedCount',
+    [],
+    contractAddress
+  )
+  const batchSize: number = useContractCall('batchSize', [], contractAddress)
+  const revealedInterval: number = useContractCall(
+    'revealedInterval',
+    [],
+    contractAddress
+  )
+  const lastRevealed: number = useContractCall(
+    'lastRevealed',
+    [],
+    contractAddress
+  )
+  const totalSupply: number = useContractCall(
+    'totalSupply',
+    [],
+    contractAddress
+  )
   return (
     <Container maxWidth="100%" textAlign="center">
-      {isPending ? (
+      {isPending(
+        revealedCount,
+        totalSupply,
+        batchSize,
+        lastRevealed,
+        revealedInterval
+      ) ? (
         <Heading>Pending batch reveal</Heading>
       ) : (
         <>
@@ -27,4 +52,26 @@ export const RevealInfo = (props: AddressProp): JSX.Element => {
       )}
     </Container>
   )
+}
+
+function isPending(
+  revealedCount: number,
+  totalSupply: number,
+  revealBatchsize: number,
+  lastRevealed: number,
+  revealInterval: number
+) {
+  let unrevealedCount = totalSupply - revealedCount
+  if (unrevealedCount == 0) {
+    return false
+  }
+  let batchSizeCriteria = false
+  if (revealBatchsize > 0 && unrevealedCount >= revealBatchsize) {
+    batchSizeCriteria = true
+  }
+  let intervalCriteria = false
+  if (revealInterval > 0 && Date.now() - lastRevealed > revealInterval) {
+    intervalCriteria = true
+  }
+  return batchSizeCriteria || intervalCriteria
 }
